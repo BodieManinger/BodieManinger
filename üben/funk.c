c7cc9558435c569971cb005aaf7e7cccd3c688cd
BodieManinger <bodie.maninger@outlook.com>
2025-03-12 17:31:42 +0000

    f

---
+++ üben/funk.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float avg(int numb1,int numb2)
+{
+    int erg = (numb1 + numb2) % 2;
+    return erg;
+}
+
+int main(void)
+{
+    int numb1 = 4;
+    int numb2 = 5;
+
+    float avg(int numb1, int numb2);
+
+    return 0;
+}
